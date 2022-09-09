fd7b04c6646e249ef781c5a3212ffdc6885762a6
peter nzioki <petenzioki@gmail.com>
2022-09-09 13:05:59 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,17 +4,21 @@
 /**
  * main - entry point
  *
- * Return: always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int d;
-	char low;
 
 	for (d = '0'; d <= '9'; d++)
-	putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-	putchar(low);
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 
 return (0);
