a8907756b2cd924675056160872f35bf6eb5f27b
peter nzioki <petenzioki@gmail.com>
2022-09-09 05:03:53 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/** this programs prints the following strig exaclty 
+/**this programs prints the following strig exaclty 
  *programming is like building a multilingual puzzle
  */
-int main()
+int main(void)
+	i
 {
-	puts("\"Programming is like building a multilingual puzzle"
+	puts ("\"Programming is like building a multilingual puzzle"
 			);
 return(0);
 }
