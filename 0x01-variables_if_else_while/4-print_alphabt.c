96073dc8b68aec3fd875b74ff92a5794d9d3128e
peter nzioki <petenzioki@gmail.com>
2022-09-09 12:14:44 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * mian -Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low ='a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	printf("\n");
+
+return (0);
+}
