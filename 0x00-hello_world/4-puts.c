e903551a0948f648d92891ebfe632d4b5f43fec2
peter nzioki <petenzioki@gmail.com>
2022-09-09 05:07:50 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *programming is like building a multilingual puzzle
  */
 int main(void)
-	i
+
 {
-	puts( "\"Programming is like building a multilingual puzzle"
+	puts ("\"Programming is like building a multilingual puzzle"
 			);
 return(0);
 }
