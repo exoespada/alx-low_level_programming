f067622c7bd76d037b2e43fc3fafdc9d64297962
peter nzioki <petenzioki@gmail.com>
2022-09-09 12:36:42 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,16 +4,14 @@
 /**
  * main - entry point
  *
- * Return: Always 0 (succes)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-	printf("%d", i);
-	printf("\n");
-
-
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
 return (0);
 }
