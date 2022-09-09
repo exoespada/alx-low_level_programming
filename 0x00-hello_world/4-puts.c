83c5f8499224fd836c35757e25ad95e0ae9d361f
peter nzioki <petenzioki@gmail.com>
2022-09-09 05:55:03 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * Description: the main function will print out the string to the stdout.
- * 
  * Return zero if successful.
  */
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle"
-);
+			);
 return(0);
 }
 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * description: the main function will print the string.
+ * betyy style is a bitch.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+			);
+return(0);
+}
