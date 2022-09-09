7ebbb3aec5091863adafebb195eac0cc9d71ea0e
peter nzioki <petenzioki@gmail.com>
2022-09-09 04:50:15 -0700

    c program with put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** this programs prints the following strig exaclty 
+ *
+ *\"Programming is like building a multilingual puzzle
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle"
+	);
+return(0);
+}
+
+
