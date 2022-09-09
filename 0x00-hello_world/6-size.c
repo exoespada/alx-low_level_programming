2d572e1c728c7ff6122b550d5fe3284f991cfeb4
peter nzioki <petenzioki@gmail.com>
2022-09-09 06:41:52 -0700

    confusing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - will be the entry point for the c program.
+ * return (0) - means that the programm ahs run successfuly.
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n",sizeof(char));
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n",sizeof(float));
+	return (0)
+}
+
