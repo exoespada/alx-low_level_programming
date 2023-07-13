c9621009302a169d0f94d3e42a0d7db9c5b00332
exoespada <petenzioki@gmail.com>
2023-07-13 12:52:56 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+
+#include <stdio.h>
+
+/* Main function */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
+
