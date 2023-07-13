87fe68ed89b714d1cd2d99e486e0936465edd9e1
exoespada <petenzioki@gmail.com>
2023-07-13 12:27:54 -0700

    put the first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+/*
+ * 4-puts.c
+ * Description: this functions prints a string followed by a new line
+ * Author: Peter Pierre Nzioki (Exoespada)
+ * Year: 2023
+ * License: MIT
+ * Written for ALX software engineering program.
+ *
+ * Copyright (c) 2023 Peter Pierre Nzioki
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char word[] = "\"Programming is like building a multilingual puzzle";
+    puts(word);
+
+    return (0);
+}
+
