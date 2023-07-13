56dfe2e034253c388dbc5614ac0931dec39be27c
exoespada <petenzioki@gmail.com>
2023-07-13 12:45:42 -0700

    corrections on betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,9 @@
-/*
- * 4-puts.c
- * main - main function prints a string folloed by new line.
- * Returns 0 after a successful run.
- * Description: this functions prints a string followed by a new line
- * Author: Peter Pierre Nzioki (Exoespada)
- * Year: 2023
- * License: MIT
- * Written for ALX software engineering program.
- *
- * Copyright (c) 2023 Peter Pierre Nzioki
- * All rights reserved.
- */
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 
 #include <stdio.h>
 
