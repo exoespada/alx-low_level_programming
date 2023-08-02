ff359abd39cb8cefc726d21c9737c5c0a48874bd
exoespada <petenzioki@gmail.com>
2023-08-02 08:48:37 -0700

    recurssion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,20 +9,14 @@
 
 int factorial(int n)
 {
-	int fact;
+	int next;
 
 	if (n == 0)
-	{
 		return (1);
-	}
 	else if (n < 0)
-	{
-		return (-1);
-	}
-	else
-	{
-		next = factorial(n - 1);
-		return (n * fact);
-	}
+	return (-1);
+
+	next = factorial(n - 1);
+	return (n * next);
 }
 
