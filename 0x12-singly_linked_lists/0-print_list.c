f072134e4189bef0aad695bbfd903bda5a1922af
exoespada <petenzioki@gmail.com>
2023-08-24 11:21:31 -0700

    simplify function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,25 @@
 #include "lists.h"
 
 /**
-* print_list - prints all the elements of a list
-*
-* @h:pointer to the list
-*
-*Return: the number of nodes
-*/
-
+ * print_list - prints all the elements of a list
+ * @h: pointer to the list
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-const list_t *cursor = h;
-size_t count = 0;
+    size_t node_counter = 0;
 
-while (cursor != NULL)
-{
-if (cursor->str != NULL)
-printf("[%d] %s\n", cursor->len, cursor->str);
-else
-printf("[0] (nil)\n");
-count += 1;
-cursor = cursor->next;
+    while (h != NULL)
+    {
+        if (h->str)
+            printf("[%d] %s\n", h->len, h->str);
+        else
+            printf("[0] (nil)\n");
+
+        node_counter++;
+        h = h->next;
+    }
+
+    return node_counter;
 }
 
-return (count);
-}
