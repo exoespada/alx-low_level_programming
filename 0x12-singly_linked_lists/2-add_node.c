55a9a79b9a013ed44bca228ff28fc0c211b91777
exoespada <petenzioki@gmail.com>
2023-08-28 01:23:32 -0700

    more linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,13 +15,12 @@ list_t *add_node(list_t **head, const char *str)
 
     new_node = malloc(sizeof(list_t));
     if (!new_node)
-        return NULL;
-
+        return (NULL);
     new_node->str = strdup(str);
     if (!new_node->str)
     {
         free(new_node);
-        return NULL;
+        return (NULL);
     }
 
     while (str[len])
@@ -31,6 +30,6 @@ list_t *add_node(list_t **head, const char *str)
     new_node->next = *head;
     *head = new_node;
 
-    return new_node;
+    return (new_node);
 }
 
